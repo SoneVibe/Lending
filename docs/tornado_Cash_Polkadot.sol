1484098f4fca699153e18a490d33a599dbfda7a4
SoneVibe <amilgaoul892@gmail.com>
2026-03-13 21:33:27 -0400

    reminder tornado cash build on polkadot
    note to remember cash mixer

---
+++ docs/tornado_Cash_Polkadot.sol
@@ -0,0 +1,12 @@
+// IERC20-precompile.sol
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+//Coming soon.....
